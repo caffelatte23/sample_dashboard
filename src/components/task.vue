e688b6567d3e3cf691749ab3bc3b0685467292a8
CaffeLatte23 <CaffeLatte623@gmail.com>
2021-11-08 09:47:39 +0900

    test

---
+++ src/components/task.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+      <el-row>
+        <TaskBox :job_status="0" />
+      </el-row>
+    </div>
+</template>
+
+<script>
+import TaskBox from "./taskBox.vue";
+
+export default {
+    name: "Task",
+    components: {TaskBox},
+}
+</script>
+
+<style>
+
+</style>
